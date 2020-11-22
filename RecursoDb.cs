4998c0ebf705b52a3b0c831ee95098053e00e14f
keki212 <65632026+keki212@users.noreply.github.com>
2020-11-22 16:07:50 -0500

    Add files via upload

---
+++ RecursoDb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GestionPedidos
+{
+    class RecursoDb
+    {
+        SqlConnection conexionDb;
+        string conexion;
+
+        public RecursoDb()
+        {
+            conexion = ConfigurationManager.ConnectionStrings["GestionPedidos.Properties.Settings.GestionPedidosConnectionString"].ConnectionString;
+            
+        }
+
+        public string conectar()
+        {
+            conexionDb = new SqlConnection(conexion);
+
+            return conexionDb.ToString();
+            
+        }
+       
+    }
+}
